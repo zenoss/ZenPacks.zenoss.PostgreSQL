a7466e8e0733b2d1558619b4ac65e9af5ec86c60
Chet Luther <cluther@zenoss.com>
2011-05-27 10:05:15 -0500

    ZenPack skeleton.

---
+++ ZenPacks/zenoss/PostgreSQL/__init__.py
@@ -0,0 +1,37 @@
+###########################################################################
+#
+# This program is part of Zenoss Core, an open source monitoring platform.
+# Copyright (C) 2011, Zenoss Inc.
+#
+# This program is free software; you can redistribute it and/or modify it
+# under the terms of the GNU General Public License version 2 or (at your
+# option) any later version as published by the Free Software Foundation.
+#
+# For complete information please visit: http://www.zenoss.com/oss/
+#
+###########################################################################
+
+import logging
+log = logging.getLogger('zen.PostgreSQL')
+
+from Products.ZenModel.ZenPack import ZenPack as ZenPackBase
+
+class ZenPack(ZenPackBase):
+    def install(self, app):
+        super(ZenPack, self).install(app)
+        self.symlinkPlugin()
+
+    def remove(self, app, leaveObjects=False):
+        if not leaveObjects:
+            self.removePluginSymlink()
+
+        super(ZenPack, self).remove(app, leaveObjects=leaveObjects)
+
+    def symlinkPlugin(self):
+        # TODO: Implementation.
+        pass
+
+    def removePluginSymlink(self):
+        # TODO: Implementation.
+        pass
+
