433784c80e1112513b2e5764beb31fe3162b774f
Oleksandr Dubrovyk <odubrovyk@zenoss.com>
2023-08-30 15:11:04 +0000

    Move changes from develop to hotfix 1.0.13
    Implements ZPS-8554.
    
    Move changes in scope of the ticket from develop to hotfix.

--- ZenPacks/zenoss/PostgreSQL/modeler/plugins/zenoss/PostgreSQL.py
+++ ZenPacks/zenoss/PostgreSQL/modeler/plugins/zenoss/PostgreSQL.py
@@ -12,6 +12,8 @@
 ###########################################################################
 
 import logging
+import re
+
 log = logging.getLogger('zen.PostgreSQL')
 
 from Products.DataCollector.plugins.CollectorPlugin import PythonPlugin
@@ -20,6 +22,7 @@ from Products.ZenUtils.Utils import prepId
 
 from ZenPacks.zenoss.PostgreSQL.util import PgHelper
 
+
 class PostgreSQL(PythonPlugin):
     deviceProperties = PythonPlugin.deviceProperties + (
         'zPostgreSQLPort',
@@ -27,6 +30,7 @@ class PostgreSQL(PythonPlugin):
         'zPostgreSQLPassword',
         'zPostgreSQLUseSSL',
         'zPostgreSQLDefaultDB',
+        'zPostgreSQLTableRegex',
     )
 
     def collect(self, device, unused):
@@ -39,6 +43,7 @@ class PostgreSQL(PythonPlugin):
             device.zPostgreSQLDefaultDB)
 
         results = {}
+        pattern = re.compile(getattr(device, 'zPostgreSQLTableRegex', '').strip())
 
         log.info("Getting database list")
         try:
@@ -55,8 +60,15 @@ class PostgreSQL(PythonPlugin):
 
             log.info("Getting tables list for {0}".format(dbName))
             try:
-                results['databases'][dbName]['tables'] = \
-                    pg.getTablesInDatabase(dbName)
+
+                tables = pg.getTablesInDatabase(dbName)
+                if pattern.pattern:
+                    for key in tables.keys():
+                        if not pattern.match(key):
+                            del tables[key]
+
+                results['databases'][dbName]['tables'] = tables
+
             except Exception, ex:
                 log.warn("Error getting tables list for {0}: {1}".format(
                     dbName, ex))
@@ -69,7 +81,7 @@ class PostgreSQL(PythonPlugin):
         if results is None:
             return None
 
-        maps = [ self.objectMap(dict(setPostgreSQL=True)) ]
+        maps = [self.objectMap(dict(setPostgreSQL=True))]
 
         databases = []
         for dbName, dbDetail in results['databases'].items():
